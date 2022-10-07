3a5fc2212a6a3b58434f5485e4485c84a0917e94
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-07 14:11:01 -0300

    router structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { RelayEnvironmentProvider } from "react-relay/hooks";
+import RelayEnv from "./services/RelayEnv";
+
+import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Router>
+        <RelayEnvironmentProvider environment={RelayEnv}>
+          <Suspense fallback={"loading..."}>
+            <Routes></Routes>
+          </Suspense>
+        </RelayEnvironmentProvider>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
