6dc0e080023b3ab43eb1462bcb3969544cf4a96b
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-07 13:57:34 -0300

    relay types

---
+++ src/services/RelayEnv.ts
@@ -0,0 +1,30 @@
+import { Environment, Network, RecordSource, Store } from "relay-runtime";
+
+function fetchQuery(operation: any, variables: any) {
+  return fetch("http://localhost:4000/graphql", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer",
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+const network = Network.create(fetchQuery);
+
+const source = new RecordSource();
+const store = new Store(source);
+
+const env = new Environment({
+  network,
+  store,
+});
+
+export default env;
