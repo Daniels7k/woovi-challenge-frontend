4c847d895fc645985bc60749d103114e316102ad
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-11 14:54:43 -0300

    despesas query implemented

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,30 @@
 import React from "react";
+
 import GraphicsComponent from "../../components/GraphicsComponent";
 import Table from "../../components/Table";
+
+import { useLazyLoadQuery } from "react-relay";
+import graphql from "babel-plugin-relay/macro";
 
 import style from "./home.module.scss";
 
 const Home = () => {
+  const response: any = useLazyLoadQuery(
+    graphql`
+      query HomeQuery {
+        despesas {
+          ...Table_table
+        }
+      }
+    `,
+    {},
+  );
+
+  console.log(response);
   return (
     <div className={style.container}>
       <GraphicsComponent />
-      <Table />
+      <Table data={response.despesas} />
     </div>
   );
 };
